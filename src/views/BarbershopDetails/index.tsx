b568ab1a91a29139fe39a6c854e02c6ce68e90f3
William Reticena <william.wsd43@gmail.com>
2026-03-07 13:09:04 -0300

    Enhance BarbershopDetails component with additional information display and improved layout

--- src/views/BarbershopDetails/index.tsx
+++ src/views/BarbershopDetails/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from 'react-router';
-import { Box, Container, HStack, Icon } from '@chakra-ui/react';
-import { ArrowLeft } from 'lucide-react';
+import { Box, Button, Card, Container, DataList, HStack, Icon, Text } from '@chakra-ui/react';
+import { ArrowLeft, Calendar, Clock, Phone } from 'lucide-react';
 
 import { useBarbershopDetails } from '../../hooks/useBarbers';
 import { BarbershopDetailsHeader } from '../../shared/BarbershopDetailsHeader';
@@ -13,23 +13,97 @@ export function BarbershopDetails() {
   console.log('data', data);
 
   return (
-    <Container>
-      <HStack alignItems='center' onClick={() => navigate(-1)}>
-        <Icon>
-          <ArrowLeft />
-        </Icon>
-        Voltar
-      </HStack>
+    <Box>
+      <Container>
+        <Container maxW='4xl' py='6'>
+          <HStack alignItems='center' mb='4'>
+            <Button variant='ghost' size='xs' onClick={() => navigate(-1)}>
+              <ArrowLeft />
+              Voltar
+            </Button>
+          </HStack>
 
-      {!isLoading ? (
-        <BarbershopDetailsHeader
-          title={{ nmBarbershop: data?.name, dsFontSize: '2xl' }}
-          inOpen={data?.isOpen}
-          description={{ nmNeighborhood: data?.neighborhood, nmCity: data?.city, dsFontSize: 'md' }}
-        />
-      ) : (
-        <p>Loading...</p>
-      )}
-    </Container>
+          {!isLoading ? (
+            <BarbershopDetailsHeader
+              title={{ nmBarbershop: data?.name, dsFontSize: '2xl' }}
+              inOpen={data?.isOpen}
+              description={{ nmNeighborhood: data?.neighborhood, nmCity: data?.city, dsFontSize: 'md' }}
+            />
+          ) : (
+            <p>Loading...</p>
+          )}
+        </Container>
+      </Container>
+
+      <Box height='1px' backgroundColor='gray.200' mt='-2' mb='4' />
+
+      <Container maxW='4xl' py='6'>
+        <Box display='flex' gap='4'>
+          <Card.Root width='33%' borderRadius='12px'>
+            <Card.Body>
+              <Box display='flex' alignItems='center' gap='4'>
+                <Icon boxSize='5' color='#F9941F' mb='1'>
+                  <Phone />
+                </Icon>
+
+                <Box>
+                  <Text fontSize='sm' color='gray.500'>
+                    TELEFONE
+                  </Text>
+                  <Text fontWeight='bold'>{data?.phone}</Text>
+                </Box>
+              </Box>
+            </Card.Body>
+          </Card.Root>
+
+          <Card.Root width='33%' borderRadius='12px'>
+            <Card.Body>
+              <Box display='flex' alignItems='center' gap='4'>
+                <Icon boxSize='5' color='#F9941F' mb='1'>
+                  <Clock />
+                </Icon>
+
+                <Box>
+                  <Text fontSize='sm' color='gray.500'>
+                    FECHA ÀS
+                  </Text>
+                  <Text fontWeight='bold'>{data?.closesAt}</Text>
+                </Box>
+              </Box>
+            </Card.Body>
+          </Card.Root>
+
+          <Card.Root width='33%' borderRadius='12px'>
+            <Card.Body>
+              <Box display='flex' alignItems='center' gap='4'>
+                <Icon boxSize='5' color='#F9941F' mb='1'>
+                  <Calendar />
+                </Icon>
+
+                <Box>
+                  <Text fontSize='sm' color='gray.500'>
+                    PRÓXIMO HORÁRIO
+                  </Text>
+                  <Text fontWeight='bold'>{new Date(data?.nextAvailableSlot).toLocaleString()}</Text>
+                </Box>
+              </Box>
+            </Card.Body>
+          </Card.Root>
+        </Box>
+
+        <Text mt='6' fontSize='2xl'>
+          Serviços
+        </Text>
+
+        <DataList.Root divideY='1px'>
+          {data?.services.map((s) => (
+            <DataList.Item key={s.id} pt='4'>
+              <DataList.ItemLabel>{s.name}</DataList.ItemLabel>
+              <DataList.ItemValue>{s.durationMinutes}</DataList.ItemValue>
+            </DataList.Item>
+          ))}
+        </DataList.Root>
+      </Container>
+    </Box>
   );
 }
