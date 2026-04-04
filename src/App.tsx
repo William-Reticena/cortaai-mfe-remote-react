b0dc1ba5b87d9ee83f0d096d88b1b3bf2aeb9aed
William Reticena <william.wsd43@gmail.com>
2026-04-04 12:49:17 -0300

    Refactor routing setup to use createRouter function and improve import organization in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useMemo } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router';
+
 import './App.css';
 import './index.css';
-import { RouterProvider } from 'react-router';
-import { router } from './routes';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRouter } from './routes';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const router = useMemo(() => createRouter(), []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,22 +2,24 @@ import { createBrowserRouter } from 'react-router';
 import { BarbershopListing } from '../views/BarbershopListing';
 import { BarbershopDetails } from '../views/BarbershopDetails';
 
-export const router = createBrowserRouter(
-  [
+export function createRouter() {
+  return createBrowserRouter(
+    [
+      {
+        index: true,
+        element: <BarbershopListing />,
+      },
+      {
+        path: 'barbershop/:id',
+        element: <BarbershopDetails />,
+      },
+      {
+        path: '*',
+        element: <h1>404 Not Found</h1>,
+      },
+    ],
     {
-      index: true,
-      element: <BarbershopListing />,
+      basename: '/c',
     },
-    {
-      path: 'barbershop/:id',
-      element: <BarbershopDetails />,
-    },
-    {
-      path: '*',
-      element: <h1>404 Not Found</h1>,
-    },
-  ],
-  {
-    basename: '/c',
-  },
-);
+  );
+}
