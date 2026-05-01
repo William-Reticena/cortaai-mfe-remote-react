8baee3ee362efd1b973adff09451c97ea81d382c
William Reticena <william.wsd43@gmail.com>
2026-05-01 17:10:40 -0300

    Add versioning to API endpoints and implement authorization token handling in API client

--- src/api/BarbersApi.ts
+++ src/api/BarbersApi.ts
@@ -4,24 +4,19 @@ import api from './client';
 
 export class BarbersApi {
   static async getListBarbershops() {
-    const response = await api.get<BarbershopListResponse[]>('/barbershops');
+    const response = await api.get<BarbershopListResponse[]>('/v1/barbershops');
 
     return response.data.map((item) => new BarbershopListResponse(item));
   }
 
   static async getBarbershopDetails(id: number) {
-    const response = await api.get<BarbershopDetailsResponse>(`/barbershops/${id}`);
+    const response = await api.get<BarbershopDetailsResponse>(`/v1/barbershops/${id}`);
 
     return new BarbershopDetailsResponse(response.data);
   }
 
   static async scheduleAppointment(request: CreateAppointmentRequest) {
-    const response = await api.post<CreateAppointmentResponse>('/appointments', request, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 1,
-      },
-    });
+    const response = await api.post<CreateAppointmentResponse>('/v1/appointments', request);
 
     return new CreateAppointmentResponse(response.data);
   }
